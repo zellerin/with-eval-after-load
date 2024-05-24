d5eeb8aad5098122f33232523607c2e27f372de7
Tomáš Zellerin <tomas@zellerin.cz>
2024-05-24 12:31:58 +0200

    More doc texts.

--- doc.lisp
+++ doc.lisp
@@ -8,7 +8,14 @@ anything like that in Common Lisp.
 
 This package imitates the with-eval-after-load macro."
 
-  "Quick start example: Drakma by default considers Javascript to be a binary
+  (@example mgl-pax:section)
+  (@dictionary mgl-pax:section)
+  (@bugs mgl-pax:section)
+)
+
+(mgl-pax:defsection @example
+    (:title "Quick start example")
+  " Drakma by default considers Javascript to be a binary
 format. I want it to handle it as text. This can be done by setting
 DRAKMA:*TEXT-CONTENT-TYPES* appropriately.
 
@@ -17,18 +24,22 @@ So I have this in my ~/.sbclrc:
 ``` lisp
 (with-eval-after-load (\"drakma\" drakma)
   (setf #:*text-content-types*
-        (list* '(\"application\" . \"javascript\") '(\"application\" . \"json\") '(\"text\" . nil))))
+        (list '(\"application\" . \"javascript\") '(\"application\" . \"json\") '(\"text\" . nil))))
 ```
 
 It does not load Drakma and its dependencies, but it makes sure that when Drakma is loaded eventually, it will have it set. Note that as DRAKMA package does not exist yet, I cannot use it to name a symbol - so the package name as a parameter and #: in the setf.
 
-The package also provides an autoload feature that is kind of experimental."
-  (@dictionary mgl-pax:section))
+The package also provides an autoload feature that is kind of experimental.")
 
 (mgl-pax:defsection @Dictionary
     (:title "Dictionary")
   (with-eval-after-load mgl-pax:macro)
   (*root-package* variable)
   (define-autoload mgl-pax:macro))
+
+(mgl-pax:defsection @bugs
+    (:title "Known bugs")
+  "This is in \"good-enough-for-me\" state."
+  "It abuses ASDF:PERFORM :AFTER method. If some system uses it, things may break. That said, I did not see it defined on the systems I use.")
 
 (mgl-pax:update-asdf-system-readmes @overview "with-eval-after-load")
